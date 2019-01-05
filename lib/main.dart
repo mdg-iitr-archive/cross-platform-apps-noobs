f6e88273ae10080d5daed4e0c482b58edb900c57
Mansi449 <mansi10022000@gmail.com>
2019-01-06 01:27:44 +0530

    add like button

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'FB Reaction',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('FB Reaction'),
         ),
         body: Center(
-          child: Text('Hello World'),
-        ),
+            child: new OutlineButton(
+                onPressed: null,
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    LikeImage(),
+                    LikeText(),
+                  ],
+                ),
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(
+                      1.0,
+                    )))),
       ),
     );
+  }
+}
+
+class LikeImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/ic_like.png');
+    Image image = Image(image: assetImage);
+    // Image thumbnail = copyResize(image, 120);
+    return Container(height: 50.0, width: 50.0, child: image);
+  }
+}
+
+class FillLikeImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/ic_like_fill.png');
+    Image image = Image(image: assetImage);
+    // Image thumbnail = copyResize(image, 120);
+    return Container(height: 50.0, width: 50.0, child: image);
+  }
+}
+
+class LikeText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Text txt = new Text('Like');
+
+    return Container(child: txt);
   }
 }
